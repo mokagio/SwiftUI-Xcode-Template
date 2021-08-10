247643169de8dce5c58666f90843e5d6ac77833a
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-11 09:59:53 +1000

    Track Xcode 12.5.1 "App" template with SwiftUI life cycle

---
+++ SwiftUI-Xcode-Template/SwiftUI_Xcode_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Xcode_TemplateApp.swift
+//  SwiftUI-Xcode-Template
+//
+//  Created by Gio on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Xcode_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
